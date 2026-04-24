e9b02ae5647676770b011957a2ebf855865fd8ce
Yosswan <yosswan24@gmail.com>
2026-04-24 09:53:47 -0400

    acomoda recarga de datos de usuario cuando no está verificado

--- hooks/user/index.ts
+++ hooks/user/index.ts
@@ -11,9 +11,13 @@ export const useUser = (fetchData = false) => {
 	let isLoading: boolean = false;
 	let refetch: (options?: RefetchOptions | undefined) => Promise<QueryObserverResult<any, unknown>>|Promise<void> = async () => {};
 	if (fetchData) {
-		({ data, isLoading, refetch } = useQuery<any>([GET_USER], () =>
-			ProfilApiService.getUser()
-		, { cacheTime: 10000, staleTime: 10000 }));
+		({ data, isLoading, refetch } = useQuery<any>(
+			[GET_USER],
+			() => ProfilApiService.getUser(),
+			{
+				refetchInterval: !session?.user?.verificado ? 3000 : false
+			}
+		));
 		if (session?.user?.email && !session?.user.verificado && data?.user?.verificado) {
 			signIn("credentials", {
 				newData: JSON.stringify({ user: { verificado: true }}),
